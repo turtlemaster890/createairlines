bd141b9cea2a246c33cc8e5531703949957f2a03
BuildTools <unconfigured@null.spigotmc.org>
2023-02-05 11:29:40 -0500

    added blocks and translations

--- src/main/java/org/turtlemaster890/createairlines/createairlines/block/ModBlocks.java
+++ src/main/java/org/turtlemaster890/createairlines/createairlines/block/ModBlocks.java
@@ -4,11 +4,14 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import org.turtlemaster890.createairlines.createairlines.Createairlines;
+import org.turtlemaster890.createairlines.createairlines.item.ModCreativeTab;
 import org.turtlemaster890.createairlines.createairlines.item.ModItems;
 
 import java.util.function.Supplier;
@@ -16,6 +19,13 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Createairlines.MODID);
+
+    public static final RegistryObject<Block> BELT_COVER = registerBlock("belt_cover", () -> new Block(BlockBehaviour.Properties.of(Material.GLASS).requiresCorrectToolForDrops()), ModCreativeTab.CREATE_AIRLINES_TAB);
+
+    public static final RegistryObject<Block> BELT_XRAY_TUNNEL = registerBlock("xray_tunnel", () -> new Block(BlockBehaviour.Properties.of(Material.GLASS).requiresCorrectToolForDrops()), ModCreativeTab.CREATE_AIRLINES_TAB);
+
+    public static final RegistryObject<Block> METAL_DETECTOR_PART = registerBlock("metal_detector_part", () -> new Block(BlockBehaviour.Properties.of(Material.GLASS).requiresCorrectToolForDrops()), ModCreativeTab.CREATE_AIRLINES_TAB);
+
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
